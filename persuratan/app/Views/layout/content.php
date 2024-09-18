7bf852be85f26b42e064a76181352d4e53845da5
Muhammad Raihan Azzaidan <110644584+raihanazzaidan@users.noreply.github.com>
2024-09-18 08:59:59 +0700

    Delete persuratan directory

--- persuratan/app/Views/layout/content.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!empty($view)) {
-    echo view($view);
-} else {
-    echo "<h1>Halaman tidak tersedia</h1>";
-}
--- persuratan/app/Views/layout/template.php
+++
@@ -1,3 +0,0 @@
-<?php echo view('layout/header') ?>
-<?php echo view('layout/body') ?>
-<?php echo view('layout/footer') ?>
